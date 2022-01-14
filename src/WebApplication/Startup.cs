1e41de845693d802dd56c9622d768ff54de48121
marcioeric <marcio.eric.nicolau@gmail.com>
2022-01-14 11:46:31 -0300

    Switch from local to CDN

--- src/WebApplication/Startup.cs
+++ src/WebApplication/Startup.cs
@@ -23,7 +23,12 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+#if DEBUG
+                .AddRazorRuntimeCompilation();
+#elif !DEBUG
+            ;
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
